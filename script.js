7ace2e73940442455bb70bd7a4b8f3bdc47215b1
Piyush Harsh <76912417+piyush-harsh-15@users.noreply.github.com>
2021-11-17 14:48:30 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 fetch('https://cat-fact.herokuapp.com/facts/random')
     .then(data => data.json())
     .then(factData => {
-        const factText = factData.attachments[0].text;
-     const factElement = document.getElementById('factElement');
+        const factText = factData.text;
+        const factElement = document.getElementById('factElement');
 
         factElement.innerHTML = factText;
     })
